5aab6383cdd8942aae7fc09e6974a2943bfff377
mobin <dota.is.me.20@gmail.com>
2024-12-21 03:32:30 +0330

    header fixed

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -12,7 +12,7 @@ const Header = ({darkMode, setDarkMode, landingPageHandler}) => {
 
   return (
       <div
-          className='fixed top-0 right-0 left-0 flex items-center justify-between gap-10 z-10 bg-light-color dark:bg-dark-color w-[1440px] p-4 duration-300 mx-auto'>
+          className='fixed top-0 right-0 left-0 flex items-center justify-between gap-10 z-10 bg-light-color dark:bg-dark-color max-w-[1440px] p-4 duration-300 mx-auto'>
           <div className='flex items-center gap-4 '>
               <span className='text-[#F79413]'>
               CoinSphere
