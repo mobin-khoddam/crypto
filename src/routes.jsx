941464e73fc83f7246e8b5b67bfa31c934072495
mobin <dota.is.me.20@gmail.com>
2024-12-31 13:27:01 +0330

    build

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,32 +1,35 @@
-import { createBrowserRouter } from "react-router-dom";
-import CryptoPage from "./pages/CryptoPage.jsx";
+import {createBrowserRouter} from "react-router-dom";
+import CryptoPage from "./pages/./CryptoPage.jsx";
 import AboutCurrency from "./pages/AboutCurrency.jsx";
+import Header from "./component/Header/Header.jsx";
 import HomePage from "./pages/HomePage.jsx";
-import Layout from "./Layout.jsx";
 
 export const router = createBrowserRouter([
     {
-        path: '/',
-        element: (
-            <Layout>
-                <HomePage />
-            </Layout>
-        ),
+      path: '/',
+      element: (
+          <>
+              <Header />
+              <HomePage />
+          </>
+      ),
     },
     {
         path: "/page/:page",
         element: (
-            <Layout>
-                <CryptoPage />
-            </Layout>
+            <>
+                <Header/>
+                <CryptoPage/>
+            </>
         )
     },
     {
         path: "/Currency/:coin",
         element: (
-            <Layout>
-                <AboutCurrency />
-            </Layout>
+            <>
+                <Header/>
+                <AboutCurrency/>
+            </>
         )
     }
-]);
+])
